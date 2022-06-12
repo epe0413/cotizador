39beec29a66fd14e1b05d6684ee4fef66e26829d
Everth Paredes <epe0413@gmail.com>
2022-06-12 15:24:48 -0600

    Validando Formularios

--- src/context/CotizadorProvider.jsx
+++ src/context/CotizadorProvider.jsx
@@ -9,6 +9,8 @@ const CotizadorProvider = ({children}) => {
         plan: ''
     })
 
+    const [error, setError] = useState('')
+
     const handleChangeDatos = e => {
         setDatos({
             ...datos,
@@ -20,7 +22,9 @@ const CotizadorProvider = ({children}) => {
         <CotizadorContext.Provider
             value={{
                 datos,
-                handleChangeDatos
+                handleChangeDatos,
+                error,
+                setError
             }}
         >
             {children}
